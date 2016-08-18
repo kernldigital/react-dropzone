59c7d6bf5847210cd3a5d264299791b808bfefa5
Pat Horsley <pat@kernl.co.nz>
2016-08-19 11:11:45 +1200

    filters max/minSize props from output <div>

--- src/index.js
+++ src/index.js
@@ -217,7 +217,8 @@ class Dropzone extends React.Component {
     }
 
     // Remove custom properties before passing them to the wrapper div element
-    const customProps = ['disablePreview', 'disableClick', 'onDropAccepted', 'onDropRejected'];
+    const customProps = ['disablePreview', 'disableClick', 'maxSize',
+                         'minSize', 'onDropAccepted', 'onDropRejected'];
     const divProps = { ...props };
     customProps.forEach(prop => delete divProps[prop]);
 
